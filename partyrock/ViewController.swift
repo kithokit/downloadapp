8d2c6b67405d1d2d055bc3ae1cdb440cb7b735bd
Kit Ho <kit.ho@iheha.com>
2016-11-18 15:56:33 +0800

    Initial Commit

---
+++ partyrock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  partyrock
+//
+//  Created by Kit 何傑禧 on 18/11/2016.
+//  Copyright © 2016 Kit 何傑禧. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
